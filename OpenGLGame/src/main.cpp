6c351087b770c3bdd3a8e8f01529f34f5a6052d5
Jetrocket88 <harrysharp625@gmail.com>
2025-02-28 12:00:42 +0000

    Ok need to lock in for english now, plus it spins xDDDDD

--- OpenGLGame/src/main.cpp
+++ OpenGLGame/src/main.cpp
@@ -118,21 +118,33 @@ int main() {
         //glm::mat4 model = glm::mat4(1.0f);
         //model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
         //veiw
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-        //projection
-       
 
         tex1.bind(0);
 
         shader.use();
-        shader.setMat4f("view", view);
 
         VAO.bind();
         for (unsigned int i = 0; i < 10; i++) {
+
+            glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+            glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+            glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+            glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+            glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+            glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+            glm::mat4 view;
+            view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+                glm::vec3(0.0f, 0.0f, 0.0f),
+                glm::vec3(0.0f, 1.0f, 0.0f));
+            const float radius = 10.0f;
+            float camX = sin(glfwGetTime()) * radius;
+            float camZ = cos(glfwGetTime()) * radius;
+            view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
+            shader.setMat4f("view", view);
             glm::mat4 projection;
-			projection = glm::perspective(glm::radians(sin((float)glfwGetTime() * 10)) + 20, 800.0f / 600.0f, 0.1f, 100.0f);
+			projection = glm::perspective(glm::radians(70.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
             shader.setMat4f("projection", projection);
             glm::mat4 model = glm::mat4(1.0f);
